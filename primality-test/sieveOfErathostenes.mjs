4b9c9683b329d23d90a68d6a8df9290efbd88ae2
Guillermo Fernandez <bengallme@gmail.com>
2021-10-11 15:43:13 -0500

    Added implementation for the sieve of erathosthenes

---
+++ primality-test/sieveOfErathostenes.mjs
@@ -0,0 +1,31 @@
+export function sieve(n) {
+  const nums = [];
+
+  for (let i = 0; i <= n; i++) {
+    if (i === 0 || i === 1) {
+      nums.push(false);
+      continue;
+    }
+    nums.push(true);
+  }
+
+  for (let i = 2; i < Math.sqrt(n); i++) {
+    if (nums[i]) {
+      for (let j = i ** 2; j <= n; j += i) {
+        nums[j] = false;
+      }
+    }
+  }
+
+  const primes = [];
+
+  for (let i in nums) {
+    if (nums[i]) {
+      primes.push(i);
+    }
+  }
+
+  return primes;
+}
+
+console.log(sieve(30));

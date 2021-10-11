185a00efb552da423d45e4c043392f686c3a9fbe
Guillermo Fernandez <bengallme@gmail.com>
2021-10-10 20:23:05 -0500

    Cleaned the implementation for quicksort algorithm

--- quicksort/quicksort.mjs
+++ quicksort/quicksort.mjs
@@ -1,18 +1,3 @@
-function simpleSort(arr) {
-  const copy = [...arr];
-
-  for (let i = 0; i < copy.length; i++) {
-    for (let j = 0; j < copy.length; j++) {
-      if (copy[i] < copy[j]) {
-        const temp = copy[i];
-        copy[i] = copy[j];
-        copy[j] = temp;
-      }
-    }
-  }
-  return copy;
-}
-
 export function quickSort(arr) {
   if (arr.length === 0) {
     return [];
@@ -39,22 +24,6 @@ export function quickSort(arr) {
   return [...quickSort(left), ...center, ...quickSort(right)];
 }
 
-const a = [];
-console.log("Filling arr with 10 000 000 elements");
-for (let i = 0; i < 100_000; i++) {
-  a.push(Math.floor(Math.random() * 100001))
-}
-console.log("Done!");
+const a = [10, 3, 0, 15, 10, 18, 2, 7, 11];
 
-console.log(a);
-console.log("Sorting arr with quicksort...");
-let start = Date.now();
 console.log(quickSort(a));
-console.log(`Done sorting. Took ${Date.now() - start}`);
-console.log("Sorting arr with simplet sort...");
-start = Date.now();
-console.log(simpleSort(a));
-console.log(`Done sorting. Took ${Date.now() - start}`);
-
-
-
